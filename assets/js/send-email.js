d29010d9c6719f9e5c5a0462277ed9bf396abd08
Gildácio Lopes <gildacioj399@gmail.com>
2025-05-08 22:19:58 -0300

    feat: add email sending

--- assets/js/send-email.js
+++ assets/js/send-email.js
@@ -1,32 +1,27 @@
-import emailjs from "https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js";
-
-emailjs.init("dWOYpwKxFm270JzLM");
-
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("subscribeForm");
   if (form) {
     form.addEventListener("submit", function (e) {
       e.preventDefault();
-      alert("Form interceptado!");
 
       const email = document.getElementById("email").value;
       const params = { email: email };
 
       const divMsg = document.getElementById("mensagem");
       if (divMsg) {
-        divMsg.textContent = `Thanks for registering, ${email}! 🎉`;
+        divMsg.textContent = `Thanks for registering, ${email}! 🎉\ncheck your inbox to receive the discount`;
         divMsg.style.color = "#fff";
       }
 
       emailjs
         .send("service_53htmdh", "template_0vv7b7j", params)
         .then(() => {
-          alert("Email enviado com sucesso!");
           form.reset();
         })
         .catch((err) => {
           console.error("Erro ao enviar:", err);
-          alert("Erro ao enviar e-mail.");
+          divMsg.textContent = `We were unable to send the email, we are sorry for what happened`;
+          divMsg.style.color = "#fff";
         });
     });
   } else {
